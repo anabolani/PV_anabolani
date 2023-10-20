52100adb6a4aa05586ccea421c20bbf413af471f
anabolani <148557975+anabolani@users.noreply.github.com>
2023-10-20 11:06:08 -0300

    Add files via upload

---
+++ python/exercicio.py
@@ -0,0 +1,12 @@
+#print("to querendo dormir")
+#ex2
+#print("ana julia bolani")
+#print("idade:15 anos")
+#print("bauru-sp")
+#print("anajuliaboalni89@gmail.com")
+#ex3
+#print("xxxxx")
+#print("x   x")
+#print("x   x")
+#print("x   x")
+#print("xxxxx")
